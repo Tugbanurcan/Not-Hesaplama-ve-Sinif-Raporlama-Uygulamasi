6c031530541fad9717056e009c4f3dc135cadcf2
Tugbanurcan <tugba.n.can@gmail.com>
2025-05-16 16:31:01 +0300

    İlk yükleme

---
+++ sinav_takip_uygulamasi/sinav_takip_uygulamasi/OgrenciKayitOl.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace sinav_takip_uygulamasi
+{
+    public partial class OgrenciKayitOl : Form
+    {
+        SinavTakipSinifi sinavTakip = new SinavTakipSinifi();
+        public OgrenciKayitOl()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKayitOl_Click(object sender, EventArgs e)
+        {
+
+            string numara = txtNumara.Text.Trim();
+            string ad = txtAd.Text.Trim();
+            string soyad = txtSoyad.Text.Trim();
+            string sifre = txtSifre.Text;
+
+            if (numara == "" || ad == "" || soyad == "" || sifre == "")
+            {
+                MessageBox.Show("Tüm alanları doldurunuz.");
+                return;
+            }
+
+            try
+            {
+                using (SqlConnection conn = sinavTakip.Baglanti())
+                {
+                    conn.Open();
+                    string sql = "INSERT INTO Ogrenciler (Numara, Ad, Soyad, Sifre) VALUES (@Numara, @Ad, @Soyad, @Sifre)";
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("@Numara", numara);
+                    cmd.Parameters.AddWithValue("@Ad", ad);
+                    cmd.Parameters.AddWithValue("@Soyad", soyad);
+                    cmd.Parameters.AddWithValue("@Sifre", sifre);
+                    cmd.ExecuteNonQuery();
+
+                    MessageBox.Show("Kayıt başarılı.");
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Hata: " + ex.Message);
+            }
+        }
+
+        private void bunifuImageButton1_Click(object sender, EventArgs e)
+        {
+            OgrenciAdminPaneli ogrPaneliSayfasi = new OgrenciAdminPaneli();
+            ogrPaneliSayfasi.Show();
+            this.Hide();
+        }
+    }
+}
+
---
+++ sinav_takip_uygulamasi/sinav_takip_uygulamasi/OgrenciSayfasi.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sinav_takip_uygulamasi
+{
+    public partial class OgrenciSayfasi : Form
+    {
+        public int OgrenciID { get; set; }
+        private SinavTakipSinifi sinavtakip;
+
+        public OgrenciSayfasi()
+        {
+            InitializeComponent();
+
+            sinavtakip = new SinavTakipSinifi();
+            this.Load += OgrenciSayfasi_Load;
+
+
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void OgrenciSayfasi_Load(object sender, EventArgs e)
+        {
+
+            if (OgrenciID > 0)
+            {
+                DataTable dt = sinavtakip.GetOgrenciNotlariByID(OgrenciID);
+                dataGridView1.DataSource = dt;
+            }
+            else
+            {
+                MessageBox.Show("Geçersiz öğrenci ID!");
+            }
+
+
+        }
+    }
+}
